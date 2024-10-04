2e160d2e1f1334550a5d4daa95c90bcf5343ce28
Kaushals <kaushals@gradelink.com>
2024-10-04 15:00:45 -0700

    Adding Headless mode for chrome

--- utils/setup.py
+++ utils/setup.py
@@ -1,19 +1,20 @@
 import os
-import unittest
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import unittest
 
 
 class BaseTest(unittest.TestCase):
 
     def setUp(self):
         chrome_options = Options()
-        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.implicitly_wait(10)
+
 
     def load_config(self):
         config = {}
