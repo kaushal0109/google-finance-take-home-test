ce293b7b3af53a099d1d808cf6e1a761270080fb
Kaushals <kaushals@gradelink.com>
2024-10-13 17:13:37 -0700

    Code clean up

--- pages/google_finance_page.py
+++ pages/google_finance_page.py
@@ -1,5 +1,3 @@
-import os
-
 from locators.locators import *
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -10,24 +8,6 @@ class GoogleFinancePage:
     def __init__(self, driver):
         self.driver = driver
         self.locator = finance
-        # self.config = self.load_config()
-
-    # def load_config(self):
-    #     config = {}
-    #
-    #     script_dir = os.path.dirname(os.path.abspath('\Take Home Exercise'))
-    #     file_path = os.path.join(script_dir, 'data.txt')
-    #
-    #     with open(file_path, 'r') as file:
-    #         for line in file:
-    #             if "=" in line:
-    #                 key, value = line.strip().split('=', 1)
-    #                 config[key] = value
-    #     return config
-    #
-    # def load_page(self):
-    #     url = self.config.get('url')
-    #     self.driver.get(url)
 
     def verify_page_loaded(self):
         WebDriverWait(self.driver, 10).until(EC.title_contains("Google Finance"))
